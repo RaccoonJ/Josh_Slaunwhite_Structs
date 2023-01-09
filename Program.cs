e134990ff12ff8ee64bcbdba609475196bf1034e
RaccoonJ <j0542351@gmail.com>
2023-01-09 14:12:51 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josh_Slaunwhite_Structs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
